8bba3bdd1ac53eaa5a3230d5e359d9b0fb93e0a8
OLEG <schastliviy.sluchay@ukr.net>
2021-12-27 22:38:46 +0200

    0.1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Input from "./components/Input";
 import { useState } from "react";
 
 function App() {
-  const [result, setResult] = useState("");
+  const [result, setResult] = useState();
   const buttonHandler = (value) => {
     decryptor(value);
   };
@@ -18,11 +18,13 @@ function App() {
         return codeArray;
       } else {
         console.log("Промокод не действительный");
-        return "Промокод не действительный";
+        setResult("Промокод не действительный");
+        return;
       }
     } else {
       console.log("Промокод не действительный");
-      return "Промокод не действительный";
+      setResult("Промокод не действительный");
+      return;
     }
   }
   // -------------------------------------------
@@ -30,69 +32,87 @@ function App() {
   function decryptor(code) {
     const promoCode = validation(code);
     console.log("Промокод", promoCode);
+    if (promoCode) {
+      // bonus 1000
+      const indexOfOdd = [];
+      const neighbours = [];
 
-    // bonus 1000
-    const indexOfOdd = [];
-    const neighbours = [];
+      promoCode.map((item, index) => {
+        // индексы нечётных
+        if (item % 2 !== 0) {
+          indexOfOdd.push(index);
+        }
+      });
 
-    promoCode.map((item, index) => {
-      // индексы нечётных
-      if (item % 2 !== 0) {
-        indexOfOdd.push(index);
+      console.log("Индексы нечётных", indexOfOdd);
+      for (let index = 0; index < indexOfOdd.length; index++) {
+        const item = indexOfOdd[index];
+
+        if (
+          indexOfOdd[index] - indexOfOdd[index - 1] === 1 ||
+          indexOfOdd[index + 1] - indexOfOdd[index] === 1
+        ) {
+          neighbours.push(item);
+
+          console.log("индексы соседних", neighbours);
+        }
       }
-    });
 
-    console.log("Индексы нечётных", indexOfOdd);
-    for (let index = 0; index < indexOfOdd.length; index++) {
-      const item = indexOfOdd[index];
+      if (neighbours.length >= 4) {
+        console.log("Минимум две пары");
+        //следующая после первой пары
+        if (promoCode[neighbours[1] + 1] % 2 === 0) {
+          console.log("следующая чётная");
 
-      if (
-        indexOfOdd[index] - indexOfOdd[index - 1] === 1 ||
-        indexOfOdd[index + 1] - indexOfOdd[index] === 1
-      ) {
-        neighbours.push(item);
-
-        console.log("индексы соседних", neighbours);
+          console.log("Ваш бонус - 1000 грн.");
+          setResult("Ваш бонус - 1000 грн.");
+          return;
+        }
       }
-    }
 
-    if (neighbours.length >= 4) {
-      console.log("Минимум две пары");
-      //следующая после первой пары
-      if (promoCode[neighbours[1] + 1] % 2 === 0) {
-        console.log("следующая чётная");
+      // bonus 100
+      let sumOfEvenNumbers = 0;
+      let sumOfOddNumbers = 0;
 
-        console.log("Ваш бонус - 1000 грн.");
-        setResult("Ваш бонус - 1000 грн.");
+      for (let item of promoCode) {
+        if (item % 2 === 0) {
+          const even = item;
+          sumOfEvenNumbers += even;
+        }
+        if (item % 2 !== 0) {
+          const odd = item;
+          sumOfOddNumbers += odd;
+        }
+      }
+
+      if (sumOfEvenNumbers > sumOfOddNumbers) {
+        console.log("Ваш бонус - 100 грн.");
+        setResult("Ваш бонус - 100 грн.");
         return;
       }
-    }
 
-    // bonus 100
-    let sumOfEvenNumbers = 0;
-    let sumOfOddNumbers = 0;
-
-    for (let item of promoCode) {
-      if (item % 2 === 0) {
-        const even = item;
-        sumOfEvenNumbers += even;
-      }
-      if (item % 2 !== 0) {
-        const odd = item;
-        sumOfOddNumbers += odd;
-      }
-    }
-
-    if (sumOfEvenNumbers > sumOfOddNumbers) {
-      console.log("Ваш бонус - 100 грн.");
-      setResult("Ваш бонус - 100 грн.");
+      console.log("Промокод не действителен");
+      setResult("Промокод не действителен");
+      return;
     }
   }
 
   return (
-    <div className="App">
-      <Input handler={buttonHandler} />
-      <p>{result}</p>
+    <div
+      className="App"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
+      <div>
+        <Input handler={buttonHandler} />
+        <p>{result}</p>
+      </div>
+      <div style={{ border: "1px solid black", padding: "5px" }}>
+        <h3 style={{ textAlign: "center" }}>Примеры промокода:</h3>
+        <p>48183276 - промокод на 100 грн</p>
+        <p>73289388 - промокод на 1000 грн</p>
+        <p>37283988 - промокод на 2000 грн</p>
+        <p>84533920 - промокод не действителен</p>
+      </div>
     </div>
   );
 }
